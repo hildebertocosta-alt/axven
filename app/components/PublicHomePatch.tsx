ad4f266fe986f2d921b01a92ae94966ebecc8cee
hildebertocosta-alt <hildebertocosta@hotmail.com>
2026-09-02 23:04:58 -0300

    fix: refine public home founder section

---
+++ app/components/PublicHomePatch.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const founderSrc = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8f
